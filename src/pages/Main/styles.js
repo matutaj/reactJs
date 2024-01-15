da18c01a1754a39a432a9228b2c774273b4bc5bb
matutaj <matutajorge2002@gmail.com>
2024-01-15 18:04:16 +0100

    feat: add regras to weg page

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
-  font-size: 24px;
-  color: ${(props) => (props.error ? "red" : "white")};
-  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
 `;
