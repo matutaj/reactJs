f0fba4a50af1f7649c80f60eebfe7562b0df6400
matutaj <matutajorge2002@gmail.com>
2024-01-15 17:36:49 +0100

    feat: add routing

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Main from './../src/pages/Main';
+import Repository from './pages/Repository';
+
+const PageRoute = () => {
+    return (
+        <BrowserRouter>
+        <Routes>
+
+            <Route path="/" Component={Main} />
+            <Route path="/repository" Component={Repository} />
+        </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default PageRoute;
