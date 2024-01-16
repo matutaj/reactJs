9c569ec7d7bbcf40da89fcddac08d96e9a4feff1
matutaj <matutajorge2002@gmail.com>
2024-01-16 20:22:56 +0100

    feat: add container  form  button submit and installed react-icons

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-import {Title} from "./styles"
+import { FaPlus, FaGithubAlt } from "react-icons/fa";
+import { Container, Form, SubmitButton } from "./styles";
 
 function Main() {
-    return <Title  >Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositóeio" />
+        <SubmitButton disable>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
